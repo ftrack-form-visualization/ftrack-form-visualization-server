e5350b772c346a3e28af23ea6b22e4903c866bd2
liaokongVFX <568250549@qq.com>
2021-09-23 23:54:52 +0800

    添加编辑表单接口和获取表单接口

--- api/forms.py
+++ api/forms.py
@@ -4,6 +4,7 @@
 @time: 2021/09/19 22:19 
 """
 from flask import jsonify, request
+import shortuuid
 
 from redprint import RedPrint
 from .utils.db_helper import Form
@@ -16,8 +17,24 @@ def get_forms():
     return jsonify({'status': 'success', 'data': Form.get_forms()})
 
 
+@forms_rp.route('/get_form', methods=['POST'])
+def get_form():
+    form_id = request.json['id']
+    return jsonify({'status': 'success', 'data': Form.get_form(form_id)})
+
+
 @forms_rp.route('/del_form', methods=['POST'])
 def delete_form():
     form_id = request.json['id']
     Form.del_form(form_id)
     return jsonify({'status': 'success', 'data': Form.get_forms()})
+
+
+@forms_rp.route('/edit_form', methods=['POST'])
+def edit_form():
+    form_id = request.json['id']
+    if not form_id:
+        form_id = shortuuid.uuid()
+
+    Form.edit_form(form_id, request.json['formName'], request.json['templates'])
+    return jsonify({'status': 'success', 'data': form_id})
--- api/utils/db_helper.py
+++ api/utils/db_helper.py
@@ -11,8 +11,17 @@ class Form(object):
 
     @classmethod
     def get_forms(cls):
-        return cls.db.query({}).all(True)
+        return cls.db.query({}, {'_id': 0}).all(True)
+
+    @classmethod
+    def get_form(cls, form_id):
+        return cls.db.query_one({'id': form_id}, {'_id': 0}) or {}
 
     @classmethod
     def del_form(cls, form_id):
         return cls.db.delete({'id': form_id})
+
+    @classmethod
+    def edit_form(cls, form_id, name, templates):
+        return cls.db.update({'name': name, 'templates': templates},
+                             {'id': form_id}, True)
